a19663d50b2473f256a2b73939489c284f84a781
PalumboValerio <61788962+PalumboValerio@users.noreply.github.com>
2020-05-09 16:04:59 +0200

    Update Utilities
    Remove not used metods

--- VenditaVeicoliSolution/VenditaVeicoliDLLProject/Utilities.cs
+++ VenditaVeicoliSolution/VenditaVeicoliDLLProject/Utilities.cs
@@ -13,53 +13,24 @@ namespace VenditaVeicoliDLLProject
     [Serializable]
     public class SerializableBindingList<T> : BindingList<T> { }
 
-    public class Utils
+    public class Utilities
     {
-        public static IEnumerable<string> ToCsv<T>(IEnumerable<T> objectlist, string separator = "|")
-        {
-            foreach (var o in objectlist)
-            {
-                FieldInfo[] fields = o.GetType().GetFields();
-                PropertyInfo[] properties = o.GetType().GetProperties();
-
-                yield return string.Join(separator, fields.Select(f => (f.GetValue(o) ?? "").ToString())
-                    .Concat(properties.Select(p => (p.GetValue(o, null) ?? "").ToString())).ToArray());
-            }
-        }
-
-        public static string ToCsvString<T>(IEnumerable<T> objectlist, string separator = "|")
-        {
-            StringBuilder csvdata = new StringBuilder();
-            foreach (var o in objectlist)
-            {
-                FieldInfo[] fields = o.GetType().GetFields();
-                PropertyInfo[] properties = o.GetType().GetProperties();
-
-                csvdata.AppendLine(string.Join(separator, fields.Select(f => (f.GetValue(o) ?? "").ToString())
-                    .Concat(properties.Select(p => (p.GetValue(o, null) ?? "").ToString())).ToArray()));
-            }
-            return csvdata.ToString();
-        }
-
-        public static void SerializeToCsv<T>(IEnumerable<T> objectlist, string pathName, string separator = "|")
-        {
-            IEnumerable<string> dataToSave = Utils.ToCsv(objectlist, separator);
-            File.WriteAllLines(pathName, dataToSave);
-        }
-
-        public static void SerializeToXml<T>(SerializableBindingList<T> objectlist, string pathName)
-        {
-            XmlSerializer x = new XmlSerializer(typeof(SerializableBindingList<T>));
-            TextWriter writer = new StreamWriter(pathName);
-            x.Serialize(writer, objectlist);
-        }
-
+        /// <summary>
+        /// Serialize an object list into Json string
+        /// </summary>
+        /// <param name="objectlist"> List to convert into Json string </param>
+        /// <param name="pathName"> Json file path </param>
         public static void SerializeToJson<T>(IEnumerable<T> objectlist, string pathName)
         {
             string json = JsonConvert.SerializeObject(objectlist, Formatting.Indented);
             File.WriteAllText(pathName, json);            
         }
 
+        /// <summary>
+        /// Parse a Json string into an object list
+        /// </summary>
+        /// <param name="pathName"> Json file path </param>
+        /// <param name="objectlist"> List to fill with json string elements </param>
         public static void ParseJsonToObject(string pathName, SerializableBindingList<Veicoli> objectlist)
         {
             string json = File.ReadAllText(pathName);
@@ -83,7 +54,13 @@ namespace VenditaVeicoliDLLProject
             }
         }
 
-        public static void CreateHtml(SerializableBindingList<Veicoli> lista, string pathName, string skeletonPathName = @".\www\index-skeleton.html")
+        /// <summary>
+        /// Create the html
+        /// </summary>
+        /// <param name="objectList"> List with datas </param>
+        /// <param name="pathName"> index.html path </param>
+        /// <param name="skeletonPathName"> Auxiliary index.html </param>
+        public static void CreateHtml(SerializableBindingList<Veicoli> objectList, string pathName, string skeletonPathName = @".\www\index-skeleton.html")
         {
             string html = File.ReadAllText(skeletonPathName);
             html = html.Replace("{{head-title}}", "AUTOVALLAURI");
@@ -91,40 +68,47 @@ namespace VenditaVeicoliDLLProject
             html = html.Replace("{{body-subtitle}}", "Le migliori occasioni al miglior prezzo!");
             
 
-            string aus = "<h3> LISTA DEI VEICOLI DISPONIBILI </h3>";
-            for (int i = 0; i < lista.Count; i++)
-                createBody(ref aus, lista, i);
+            string mainContent = "<h3> LISTA DEI VEICOLI DISPONIBILI </h3>";
+            for (int i = 0; i < objectList.Count; i++)
+                CreateBody(ref mainContent, objectList, i);
 
-            html = html.Replace("{{main-content}}", aus);
+            html = html.Replace("{{main-content}}", mainContent);
             File.WriteAllText(pathName, html);
         }
-
-        private static void createBody(ref string aus, SerializableBindingList<Veicoli> lista, int i)
+        
+        /// <summary>
+        /// Create body for the html file
+        /// </summary>
+        /// <param name="mainContent"> Content to add </param>
+        /// <param name="objectList"> List with datas </param>
+        /// <param name="i"> List data position </param>
+        private static void CreateBody(ref string mainContent, SerializableBindingList<Veicoli> objectList, int i)
         {
-            string veicolo = string.Empty;
+            string veicolo;
             string usato;
             string km0 = string.Empty;
 
-            if (lista[i].IsKmZero)
+            if (objectList[i].IsKmZero)
                 km0 = "Chilometro zero<br>";
                 
 
-            if (lista[i].IsUsato)
+            if (objectList[i].IsUsato)
                 usato = "Usato";
             else
                 usato = "Nuovo";
 
-            if (lista[i] is Moto)
+            
+            if (objectList[i] is Moto)
                 veicolo = "moto.jpg";
             else
                 veicolo = "auto.jpg";
 
-            aus += "<div class = \"veicolo\">";
-            aus += $"<img src = \"img/{veicolo}\">";
-            aus += $"<div class = \"titolo\">{lista[i].Marca} {lista[i].Modello}";
-            aus += $"<div class = \"didascalia\"> Colore: {lista[i].Colore}<br> Cilindrata: {lista[i].Cilindrata}<br> Immatricolazione: {lista[i].Immatricolazione}<br> {km0} {usato}<br> Chilometri percorsi: {lista[i].KmPercorsi}<br> Potenza: {lista[i].PotenzaKw} </div>";
-            aus += "</div>";
-            aus += "</div>";
+            mainContent += "<div class = \"veicolo\">";
+            mainContent += $"<img src = \"img/{veicolo}\">";
+            mainContent += $"<div class = \"titolo\">{objectList[i].Marca} {objectList[i].Modello}";
+            mainContent += $"<div class = \"didascalia\"> Colore: {objectList[i].Colore}<br> Cilindrata: {objectList[i].Cilindrata}<br> Immatricolazione: {objectList[i].Immatricolazione}<br> {km0} {usato}<br> Chilometri percorsi: {objectList[i].KmPercorsi}<br> Potenza: {objectList[i].PotenzaKw} </div>";
+            mainContent += "</div>";
+            mainContent += "</div>";
         }
     }
 }
